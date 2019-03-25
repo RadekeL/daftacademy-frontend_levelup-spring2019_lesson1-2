0c3744a23e347e7d9fc4cbd8d2e968a4decf68a2
Radek <radeklejman123@gmail.com>
2019-03-25 02:05:14 +0100

    added files

---
+++ src/app.js
@@ -0,0 +1,2 @@
+import {default as newDefault} from './script';  // file downloaded from daftacademy repository url: https://github.com/daftcode/daftacademy-frontend_levelup-spring2019/blob/master/prace_domowe/lekcja_1/script.js#L3
+newDefault()
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
