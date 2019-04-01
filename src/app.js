302003b282daa818ae4a75db09d192f7ceb1fcb0
Radek <radeklejman123@gmail.com>
2019-04-01 23:02:39 +0200

    completed homework 2 (styled page with rwd)

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-import {default as newDefault} from './script';  // file downloaded from daftacademy repository url: https://github.com/daftcode/daftacademy-frontend_levelup-spring2019/blob/master/prace_domowe/lekcja_1/script.js#L3
-newDefault()
+// import {default as newDefault} from './script';  // file downloaded from daftacademy repository url: https://github.com/daftcode/daftacademy-frontend_levelup-spring2019/blob/master/prace_domowe/lekcja_1/script.js#L3
+import './scss/style.scss'
+
+
+// newDefault()
+const bar = document.querySelector('.main__icon');
+bar.addEventListener('click',()=>{
+    document.querySelector('.main__nav').classList.toggle('main__nav--mobile');
+})  
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: './src/app.js',
@@ -22,6 +24,24 @@ module.exports = {
           }
         }
       }
-    ]
-  }
+    ],
+    rules: [{
+      test: /\.s(a|c)ss$/,
+      use: [
+        isProduction
+        ? MiniCssExtractPlugin.loader
+        : { loader: 'style-loader', options: { sourceMap: true } },
+        { loader: 'css-loader', options: { sourceMap: isProduction } },
+        { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+        { loader: 'sass-loader', options: { sourceMap: isProduction } }
+      ]
+      }],
+},
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output; optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
 };
